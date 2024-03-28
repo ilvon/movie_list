88152109568cc3dab0c23114ff12d75b0ec8bede
iivon <endlesslimbo@gmail.com>
2024-03-28 20:22:10 +0800

    modify the last update date when removing entry

--- rmEntry.py
+++ rmEntry.py
@@ -1,4 +1,5 @@
 from lxml import etree
+from datetime import date
 
 def remove_Entry(index: int):
     with open('./src/index.html', 'r', encoding='utf-8') as fsrc:
@@ -10,9 +11,10 @@ def remove_Entry(index: int):
     remove_row = tbody.xpath(f'./tr[@id={index}]')[0]
     remove_row_name = remove_row.xpath("./td/a[not(./text()='TMDB')]/text()")[0]
     print(f"Removing #{index}: {remove_row_name}")
-    
     tbody.remove(remove_row)
+    
     div_info_text = root.xpath('./body/div[@class="container"]/h4')
+    div_info_text[0].text = f"Last Update: {date.today().strftime('%d-%m-%Y')}"
     div_info_text[2].text = f"Total Entries: {len(root.xpath('.//tbody/tr'))}"
     
     for i, row in enumerate(root.xpath('.//tbody/tr')[index:]):
